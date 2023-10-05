dee5edb1cceb19a15f9f44bd9908153c3f0aab67
mir_faisal <mirfaisal08@gmail.comm>
2023-10-06 00:05:30 +0600

    completed diu

--- Components/DIY/DoItYours.jsx
+++ Components/DIY/DoItYours.jsx
@@ -1,8 +1,8 @@
 const DoItYours = () => {
   return (
     <>
-      <div className="h-[800px]">
-        <div className="max-w-screen-xl h-[900px] mx-auto">
+      <div className="pb-40">
+        <div className="max-w-screen-xl mx-auto">
           <h2 className="max-w-[600px] text-5xl font-semibold text-center mx-auto pt-20 pb-8">
             নিজের শেখা নিজেই গুছিয়ে নেয়ার যাত্রা শুরু হোক
           </h2>
@@ -11,55 +11,55 @@ const DoItYours = () => {
           </p>
 
           <div className="flex pt-10 flex-wrap justify-center gap-9">
-            <div className="w-[500px] border p-6 flex items-center rounded-md hover:border-green-500">
+            <div className="w-[500px] border p-6 flex items-center bg-white rounded-md hover:border-green-500">
               <img
-                width="48"
-                height="48"
-                src="https://img.icons8.com/emoji/48/backpack-emoji.png"
+                width="96"
+                height="96"
+                src="https://img.icons8.com/emoji/96/backpack-emoji.png"
                 alt="backpack-emoji"
               />
               <div className="text w-full text-left ps-3">
-                <p>ক্লাস ৫-১২</p>
-                <p>ফ্রি ভিডিও, লাইভ ক্লাস</p>
+                <p className="text-2xl py-2">ক্লাস ৫-১২</p>
+                <p className="text-md">ফ্রি ভিডিও, লাইভ ক্লাস</p>
               </div>
               <i className="fa-regular fa-chevron-right"></i>
             </div>
-            <div className="w-[500px] border p-6 flex items-center rounded-md hover:border-green-500">
+            <div className="w-[500px] border p-6 flex items-center bg-white rounded-md hover:border-green-500">
               <img
-                width="48"
-                height="48"
-                src="https://img.icons8.com/emoji/48/backpack-emoji.png"
+                width="96"
+                height="96"
+                src="https://img.icons8.com/emoji/96/backpack-emoji.png"
                 alt="backpack-emoji"
               />
               <div className="text w-full text-left ps-3">
-                <p>ক্লাস ৫-১২</p>
-                <p>ফ্রি ভিডিও, লাইভ ক্লাস</p>
+                <p className="text-2xl py-2">ক্লাস ৫-১২</p>
+                <p className="text-md">ফ্রি ভিডিও, লাইভ ক্লাস</p>
               </div>
               <i className="fa-regular fa-chevron-right"></i>
             </div>
-            <div className="w-[500px] border p-6 flex items-center rounded-md hover:border-green-500">
+            <div className="w-[500px] border p-6 flex items-center bg-white rounded-md hover:border-green-500">
               <img
-                width="48"
-                height="48"
-                src="https://img.icons8.com/emoji/48/backpack-emoji.png"
+                width="96"
+                height="96"
+                src="https://img.icons8.com/emoji/96/backpack-emoji.png"
                 alt="backpack-emoji"
               />
               <div className="text w-full text-left ps-3">
-                <p>ক্লাস ৫-১২</p>
-                <p>ফ্রি ভিডিও, লাইভ ক্লাস</p>
+                <p className="text-2xl py-2">ক্লাস ৫-১২</p>
+                <p className="text-md">ফ্রি ভিডিও, লাইভ ক্লাস</p>
               </div>
               <i className="fa-regular fa-chevron-right"></i>
             </div>
-            <div className="w-[500px] border p-6 flex items-center rounded-md hover:border-green-500">
+            <div className="w-[500px] border p-6 flex items-center bg-white rounded-md hover:border-green-500">
               <img
-                width="48"
-                height="48"
-                src="https://img.icons8.com/emoji/48/backpack-emoji.png"
+                width="96"
+                height="96"
+                src="https://img.icons8.com/emoji/96/backpack-emoji.png"
                 alt="backpack-emoji"
               />
               <div className="text w-full text-left ps-3">
-                <p>ক্লাস ৫-১২</p>
-                <p>ফ্রি ভিডিও, লাইভ ক্লাস</p>
+                <p className="text-2xl py-2">ক্লাস ৫-১২</p>
+                <p className="text-md">ফ্রি ভিডিও, লাইভ ক্লাস</p>
               </div>
               <i className="fa-regular fa-chevron-right"></i>
             </div>
