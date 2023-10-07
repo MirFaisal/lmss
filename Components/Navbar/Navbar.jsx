cb4941e1614c59ee8c9e8980126c1fce58051eae
mir_faisal <mirfaisal08@gmail.comm>
2023-10-07 10:05:42 +0600

    added logo

--- Components/Navbar/Navbar.jsx
+++ Components/Navbar/Navbar.jsx
@@ -7,14 +7,20 @@ const Navbar = () => {
       <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <NavLink to={"/"} className="flex items-center">
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8 mr-3"
-              alt="Flowbite Logo"
-            />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
-            </span>
+            <div className="flex items-center">
+              <img
+                src={"/public/logo/LOGO.png"}
+                className="h-8 mr-3"
+                alt="Logo"
+              />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                <img
+                  src={"/public/logo/Academy.png"}
+                  className="h-8 mr-3"
+                  alt="Logo"
+                />
+              </span>
+            </div>
           </NavLink>
           <button
             data-collapse-toggle="navbar-dropdown"
