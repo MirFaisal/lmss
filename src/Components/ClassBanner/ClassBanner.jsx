08f7f0a59855807909229e934314ccdae2f1dfb2
mir_faisal <mirfaisal08@gmail.comm>
2023-10-18 12:52:05 +0600

    trying to fix navbar in mobile

--- src/Components/ClassBanner/ClassBanner.jsx
+++ src/Components/ClassBanner/ClassBanner.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 const ClassBanner = ({ classRank, subtitle }) => {
+  console.log(classRank);
   return (
-    <div className="bg-gray-900 mb-14 py-10 w-full overflow-hidden">
+    <div className="bg-gray-900 mb-14 py-10 w-full overflow-hidden mt-20">
       <div className="max-w-screen-xl h-full mx-auto items-center flex justify-between relative">
         <div className="text pl-5 text-white h-full flex flex-col justify-center">
-          <h2 className="text-2xl pb-5 font-semibold">{classRank}</h2>
+          <h2 className="text-2xl pb-5 font-semibold">{ classRank }</h2>
           <p>
             লেখাপড়ায় এক ভিন্ন মাত্রা যোগ করতে থাকছে রেকর্ডেড/লাইভ প্রোগ্রামসহ
             আরো অনেক কিছু
