f034b37635bb1d34667565a537dec844da9098af
Nayan-Islam <islamnayan588@gmail.com>
2023-10-18 21:50:17 +0600

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/tw-elements/dist/js/**/*.js",
-    "./node_modules/flowbite/**/*.js",
+    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
